386274db20d3912dc0cbb8120f4dcc6d38650266
Joaquin Mestanza <jmestanza@itba.edu.ar>
2019-10-06 18:08:59 -0300

    borre uart viejos

--- Guia_4_uart/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := C:/Users/newto/LABO_DE_MICROS/Guia_4_uart/source/gpio.o
-
-LIBS :=
-
--- Guia_4_uart/Debug/source/App.d
+++
@@ -1,8 +0,0 @@
-source/App.o source/App.d: ../source/App.c ../source/board.h \
- ../source/gpio.h ../source/uart.h
-
-../source/board.h:
-
-../source/gpio.h:
-
-../source/uart.h:
