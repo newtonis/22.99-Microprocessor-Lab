b088db4094b26045dbce839ca386efe75333db8e
Joaquin Mestanza <jmestanza@itba.edu.ar>
2019-10-06 18:08:11 -0300

    borre carpetas que no usaba

--- TP2/Version Final con cambios Joaco/projects/Blink/Debug/source/App.d
+++
@@ -1,8 +0,0 @@
-source/App.o source/App.d: ../source/App.c ../source/Posicionamiento.h \
- ../source/ExternalManager.h ../source/timer.h
-
-../source/Posicionamiento.h:
-
-../source/ExternalManager.h:
-
-../source/timer.h:
