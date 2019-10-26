4251159eca348c626d057190beba0b6d6628cc77
Marcelo Regueira <mregueira@itba.edu.ar>
2019-10-26 12:47:34 -0300

    modulador v2 sin DMA

--- ADCTest/Blink/Debug/source/Modulador.d
+++ ADCTest/Blink/Debug/source/Modulador.d
@@ -1,8 +1,35 @@
 source/Modulador.o source/Modulador.d: ../source/Modulador.c \
- ../source/Modulador.h ../source/DAC.h ../source/PIT.h
+ ../source/Modulador.h ../source/ftm.h ../source/board.h ../source/gpio.h \
+ ../SDK/startup/hardware.h ../SDK/CMSIS/fsl_device_registers.h \
+ ../SDK/CMSIS/MK64F12.h ../SDK/CMSIS/core_cm4.h \
+ ../SDK/CMSIS/cmsis_version.h ../SDK/CMSIS/cmsis_compiler.h \
+ ../SDK/CMSIS/cmsis_gcc.h ../SDK/CMSIS/system_MK64F12.h \
+ ../SDK/CMSIS/MK64F12_features.h ../SDK/CMSIS/core_cm4.h
 
 ../source/Modulador.h:
 
-../source/DAC.h:
+../source/ftm.h:
 
-../source/PIT.h:
+../source/board.h:
+
+../source/gpio.h:
+
+../SDK/startup/hardware.h:
+
+../SDK/CMSIS/fsl_device_registers.h:
+
+../SDK/CMSIS/MK64F12.h:
+
+../SDK/CMSIS/core_cm4.h:
+
+../SDK/CMSIS/cmsis_version.h:
+
+../SDK/CMSIS/cmsis_compiler.h:
+
+../SDK/CMSIS/cmsis_gcc.h:
+
+../SDK/CMSIS/system_MK64F12.h:
+
+../SDK/CMSIS/MK64F12_features.h:
+
+../SDK/CMSIS/core_cm4.h:
