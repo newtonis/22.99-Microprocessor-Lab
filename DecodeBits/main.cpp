ee374e2a5802165178bc6ce6d39fb77599916986
Joaquin Mestanza <jmestanza@itba.edu.ar>
2019-10-27 12:19:54 -0300

    bugfix de decodebits

--- DecodeBits/main.cpp
+++ DecodeBits/main.cpp
@@ -45,6 +45,10 @@ static uint8_t buffer[8] = {255,255,255,255,255,255,255,255};
 static uint8_t index = 0;
 static bool is_buff_full = false;
 
+static uint8_t  status = NOT_TAKEN_SYMBOLS;
+static uint8_t a0;
+static uint8_t a1;
+
 void bufferFlush(void){
     for(int i = 0; i < sizeof(buffer) ; i++) {
         buffer[i] = 0;
@@ -74,27 +78,35 @@ bool isBufferFull(void){
     return is_buff_full;
 }
 
+void write2BuffLogic(uint8_t a0, uint8_t a1, uint8_t threshold){
+    if(!isBufferFull()){
+        writeBuffer(a0,a1,threshold);
+    }else{
+        buffer2UART();
+        bufferFlush();
+        writeBuffer(a0,a1,threshold);
+    }
+}
 
 int main() {
-    uint8_t  status = NOT_TAKEN_SYMBOLS;
-    uint8_t a0;
-    uint8_t a1;
+
     uint8_t input;
-    //uint8_t data[] ={55, 55, 100,0, 45, 52, 95, 5, 99, 0}; // esto sería 1 0 0 1 1
-    // si hay uno malo, no cmabia de estado hasta que haya uno bueno
 
-    uint8_t data[] ={55, 33, 100,0, 45, 52, 95, 5, 99, 0}; // esto sería 1 0 0 1 1
+    // la logica funciona mientras sean números validos de duty.
+    // voy a pasar los datos "on the fly" (es decir, duty tras duty)
 
-    // con el 33 entonces seria:
-    //  0 solamente (o sea una cagada)
+    uint8_t data[] ={55, 55, 100,0, 45, 52, 95, 5, 99, 0}; // esto sería 0 1 0 1 1
 
-    // arreglado seria:
-    // 1 0 1 1
+    // caso en el que viene mal el segundo y tiene que predecir en base a a0:
+    //uint8_t data[] ={55, 33, 100,0, 45, 52, 95, 5, 99, 0}; // esto sería 0 1 0 1 1
 
-    // pero estaria perdiendo el bit 55 33...
+    // caso en el que empiezo por el segundo (a1):
+    //uint8_t data[] ={55, 100, 0, 55, 45, 100, 0, 50, 50}; // esto sería 0 1 0 1 0
+    // en este caso, me re sincronizo y empieza a funcionar normal de nuevo desde 100,0
+
 
     bool message_received =  true;
-    for(int  i =0 ; i< (sizeof(data)); i++){
+    for(int  i =0 ; i< (sizeof(data)); i++){ // con esto simulo los datos on the fly!! (no son buffer, cuidado!)
         input = data[i];
         if (message_received){
             if(status==NOT_TAKEN_SYMBOLS){
@@ -107,18 +119,31 @@ int main() {
                     a1 = input;
                     status = SECOND_DC_TAKEN;
                 }else{
-                    status = NOT_TAKEN_SYMBOLS;
-                    //aca deberia escribri la respeusta al conflcito
+                    if(isNumberN(100,input,10)) { // si era valido a0 pero es el seugndo caracter
+                        if(isNumberN(50,a0, 10)){ // si de 50 pasa a 100, entonces antes era 50,50 y ahora agarro 100
+                            write2BuffLogic(50,50,10);
+                        }
+                        a0 = 100;
+                        status = FIRST_DC_TAKEN;
+                    }else if(isNumberN(50, input, 10)){ // si de 0 pasa a 50
+                        if(isZero(a0,10)){              // asumo que nates hubo un 100, 0 y ahora viene
+                            write2BuffLogic(100,0,10);  //
+                        }
+                        a0 = 50;
+                        status = FIRST_DC_TAKEN;
+                    }else{
+                        // si fue totalmente invalido el input predigo qué va a venir
+                        if(isNumberN(100,a0,10)){
+                            a1= 0;
+                        }else if(isNumberN(50,a0,10)){
+                            a1= 50;
+                        }
+                        status = SECOND_DC_TAKEN;
+                    }
                 }
             }
             if(status == SECOND_DC_TAKEN){
-                if(!isBufferFull()){
-                    writeBuffer(a0,a1,10);
-                }else{
-                    buffer2UART();
-                    bufferFlush();
-                    writeBuffer(a0,a1,10);
-                }
+                write2BuffLogic(a0,a1,10);
                 status = NOT_TAKEN_SYMBOLS;
             }
         }
